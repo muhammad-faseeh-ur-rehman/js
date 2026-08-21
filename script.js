8743c5a043e8977106f2dfc1531a60ee7cb54bbd
Muhammad Faseeh Ur Rehman <muhammadfaseehurrehman05@gmail.com>
2026-08-21 23:00:28 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,42 @@
+let username = "Fasi Ur Rehman"
+
+console.log(username);
+
+
+const userId = 1232
+
+console.log(userId);
+
+username = "ali"
+
+console.log(username);
+
+// 
+
+// data types /
+
+let isuserPass = false
+
+console.log(isuserPass);
+
+
+let userone = Symbol("ali")
+let usertwo = Symbol("ali")
+
+console.log(userone === usertwo);
+
+
+let usercity 
+console.log(usercity);
+
+let userTemp = null 
+
+
+console.log(typeof userId);
+
+
+
+
+
+
+
