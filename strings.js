810985f2e64c2d8f42c2c7b530726677f514d185
Muhammad Faseeh Ur Rehman <muhammadfaseehurrehman05@gmail.com>
2026-08-29 00:31:28 +0500

    Initial commit

---
+++ strings.js
@@ -0,0 +1,32 @@
+
+
+
+let userName = "ali"
+
+console.log(userName.length);
+
+
+console.log(userName.slice(0,2));
+
+
+let num = 343.4
+
+console.log(num.toFixed(02))
+
+let myDAte = new Date
+
+console.log(myDAte);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
