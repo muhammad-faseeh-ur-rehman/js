11c025ee4c2024bfcb12df311a789f616c999f30
Muhammad Faseeh Ur Rehman <muhammadfaseehurrehman05@gmail.com>
2026-09-02 19:21:03 +0500

    Initial commit

--- script.js
+++ script.js
@@ -1,81 +1,81 @@
-// let username = "ali";
-// console.log(username.length);
-// console.log(username.toUpperCase());
-// console.log(username.toLowerCase());
-// console.log(username.includes("a"));
-// console.log(username.startsWith("a"));
-// console.log(username.endsWith("i"));
-// console.log(username.indexOf("l"));
-// console.log(username.slice(0, 2));
-// let text = "Hello Ali";
-// console.log(text.replace("Ali", "Faseeh"));
-// let text2 = "ali ali ali";
-// console.log(text2.replaceAll("ali", "Faseeh"));
-// let name = "Ali Faseeh";
-// console.log(name.split(" "));
-// let text3 = "   Hello Ali   ";
-// console.log(text3.trim());
-// let name2 = "Ali";
-// console.log(name2.charAt(0));
+let username = "ali";
+console.log(username.length);
+console.log(username.toUpperCase());
+console.log(username.toLowerCase());
+console.log(username.includes("a"));
+console.log(username.startsWith("a"));
+console.log(username.endsWith("i"));
+console.log(username.indexOf("l"));
+console.log(username.slice(0, 2));
+let text = "Hello Ali";
+console.log(text.replace("Ali", "Faseeh"));
+let text2 = "ali ali ali";
+console.log(text2.replaceAll("ali", "Faseeh"));
+let name = "Ali Faseeh";
+console.log(name.split(" "));
+let text3 = "   Hello Ali   ";
+console.log(text3.trim());
+let name2 = "Ali";
+console.log(name2.charAt(0));
 
-// // array //
+// array //
 
-// let fruits = ["Apple", "Banana", "Mango"];
-// console.log(fruits.length);
-// fruits.push("Orange");
-// console.log(fruits);
-// fruits.pop();
-// console.log(fruits);
-// fruits.shift();
-// console.log(fruits);
-// fruits.unshift("Apple");
-// console.log(fruits);
-// console.log(fruits.slice(0, 2));
-// fruits.splice(1, 1);
-// console.log(fruits);
-// console.log(fruits.includes("Apple"));
-// console.log(fruits.indexOf("Mango"));
-// let numbers = [10, 20, 30, 40];
-// let result = numbers.find(function(num) {
-//     return num > 20;
-// });
-// console.log(result);
-// let result2 = numbers.findIndex(function(num) {
-//     return num > 20;
-// });
-// console.log(result2);
-// let doubled = numbers.map(function(num) {
-//     return num * 2;
-// });
-// console.log(doubled);
-// let filtered = numbers.filter(function(num) {
-//     return num > 20;
-// });
-// console.log(filtered);
-// let total = numbers.reduce(function(sum, num) {
-//     return sum + num;
-// }, 0);
-// console.log(total);
-// numbers.forEach(function(num) {
-//     console.log(num);
-// });
-// let checkSome = numbers.some(function(num) {
-//     return num > 30;
-// });
-// console.log(checkSome);
-// let checkEvery = numbers.every(function(num) {
-//     return num > 5;
-// });
-// console.log(checkEvery);
-// let nums = [30, 10, 40, 20];
-// nums.sort(function(a, b) {
-//     return a - b;
-// });
-// console.log(nums);
-// nums.reverse();
-// console.log(nums);
-// let names = ["Ali", "Ahmed", "Faseeh"];
-// console.log(names.join(" - "));
+let fruits = ["Apple", "Banana", "Mango"];
+console.log(fruits.length);
+fruits.push("Orange");
+console.log(fruits);
+fruits.pop();
+console.log(fruits);
+fruits.shift();
+console.log(fruits);
+fruits.unshift("Apple");
+console.log(fruits);
+console.log(fruits.slice(0, 2));
+fruits.splice(1, 1);
+console.log(fruits);
+console.log(fruits.includes("Apple"));
+console.log(fruits.indexOf("Mango"));
+let numbers = [10, 20, 30, 40];
+let result = numbers.find(function(num) {
+    return num > 20;
+});
+console.log(result);
+let result2 = numbers.findIndex(function(num) {
+    return num > 20;
+});
+console.log(result2);
+let doubled = numbers.map(function(num) {
+    return num * 2;
+});
+console.log(doubled);
+let filtered = numbers.filter(function(num) {
+    return num > 20;
+});
+console.log(filtered);
+let total = numbers.reduce(function(sum, num) {
+    return sum + num;
+}, 0);
+console.log(total);
+numbers.forEach(function(num) {
+    console.log(num);
+});
+let checkSome = numbers.some(function(num) {
+    return num > 30;
+});
+console.log(checkSome);
+let checkEvery = numbers.every(function(num) {
+    return num > 5;
+});
+console.log(checkEvery);
+let nums = [30, 10, 40, 20];
+nums.sort(function(a, b) {
+    return a - b;
+});
+console.log(nums);
+nums.reverse();
+console.log(nums);
+let names = ["Ali", "Ahmed", "Faseeh"];
+console.log(names.join(" - "));
 
 // math //
 console.log(Math.round(4.6));
--- strings.js
+++ strings.js
@@ -1,56 +1,56 @@
 
 
 
-// let userName = "ali"
+let userName = "ali"
 
-// console.log(userName.length);
-
-
-// console.log(userName.slice(0,2));
+console.log(userName.length);
 
 
-// let num = 343.4
+console.log(userName.slice(0,2));
 
-// console.log(num.toFixed(02))
 
-// let myDAte = new Date
+let num = 343.4
 
-// console.log(myDAte);
-// let Arrey = ["ali", "ahmed", "khalid", "hammad" ];
-// Arrey.push("fakhar");
-// Arrey.pop();
-// Arrey.unshift('orange')
-// Arrey.shift();
-// Arrey.splice(0,0,"grapes");
-// let num1 = [1,2,3,4,5];
-// let num2 = [6,7];
-// console.log(num1.concat(num2));
-// console.log(num1.at(2));
-// num1.copyWithin(2,4);
-// console.log(num1);
-// console.log([...Arrey.entries()]);
-// console.log(Arrey.includes("ali"));
-// console.log([...Arrey.keys()]);
-// let numbers = [1, 2, 3, 4, 5, 6];
+console.log(num.toFixed(02))
 
-// let evenNumbers = numbers.filter(function(number) {
-//     return number % 2 === 0;
-// });
-// console.log(evenNumbers);
-// console.log(num1.every(x => x % 2 === 9));
-// console.log(num2.fill(0));
-// console.log(num1.find( x => x >2));
-// console.log(num1.findLastIndex(x => x > 2));
-// flatmap
-// let a = [1, 2, 3];
-// let result = a.flatMap(x => [ x * 2]);
-// console.log(result);
-// console.log(a.join("-"));
-// console.log(result);
-// let a = [1, 2, 3, 4];
-// let result = a.reduce((sum, x ) => sum + x,0);
-// console.log(a.shift());
-// console.log(result);
+let myDAte = new Date
+
+console.log(myDAte);
+let Arrey = ["ali", "ahmed", "khalid", "hammad" ];
+Arrey.push("fakhar");
+Arrey.pop();
+Arrey.unshift('orange')
+Arrey.shift();
+Arrey.splice(0,0,"grapes");
+let num1 = [1,2,3,4,5];
+let num2 = [6,7];
+console.log(num1.concat(num2));
+console.log(num1.at(2));
+num1.copyWithin(2,4);
+console.log(num1);
+console.log([...Arrey.entries()]);
+console.log(Arrey.includes("ali"));
+console.log([...Arrey.keys()]);
+let numbers = [1, 2, 3, 4, 5, 6];
+
+let evenNumbers = numbers.filter(function(number) {
+    return number % 2 === 0;
+});
+console.log(evenNumbers);
+console.log(num1.every(x => x % 2 === 9));
+console.log(num2.fill(0));
+console.log(num1.find( x => x >2));
+console.log(num1.findLastIndex(x => x > 2));
+flatmap
+let a = [1, 2, 3];
+let result = a.flatMap(x => [ x * 2]);
+console.log(result);
+console.log(a.join("-"));
+console.log(result);
+let a = [1, 2, 3, 4];
+let result = a.reduce((sum, x ) => sum + x,0);
+console.log(a.shift());
+console.log(result);
 
 
 
